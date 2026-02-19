91ccd19ee15d9f1485dbd58bc715db725bb347d3
Vishal Kumar <vishalkumar2501kvs@gmail.com>
2026-02-19 20:20:23 +0530

    making cards to learn props

--- 03-props/src/App.jsx
+++ 03-props/src/App.jsx
@@ -1,11 +1,10 @@
-
 import Card from './components/Card'
 
 const App = () => {
   return (
     <div className ='parent'>
         <Card  user='Vishal kumar' age ={20} img='https://imgs.search.brave.com/NzzKD8UPxo9Rc_kBLS9fsiZ_mzNbUHl6my_W5nRz4kE/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMuc2Vla2xvZ28u/Y29tL2xvZ28tcG5n/LzM1LzIvaWNjLWNy/aWNrZXQtd29ybGQt/Y3VwLTIwMTktbG9n/by1wbmdfc2Vla2xv/Z28tMzUzNjAwLnBu/Zw'/>
-        <Card  user='Mrityunjay' age ={25} img ='https://imgs.search.brave.com/wCA5UJhkvLNhtBkMlFpB2NPXuV48OMt0u23BbKODGk4/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMuc2Vla2xvZ28u/Y29tL2xvZ28tcG5n/LzM2LzMvaWNjLXdv/cmxkLXQyMC1hdXN0/cmFsaWEtMjAyMC1s/b2dvLXBuZ19zZWVr/bG9nby0zNjQxODEu/cG5n'/>
+        <Card  user='Mrityunjay' age ={25}  img ='https://imgs.search.brave.com/wCA5UJhkvLNhtBkMlFpB2NPXuV48OMt0u23BbKODGk4/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMuc2Vla2xvZ28u/Y29tL2xvZ28tcG5n/LzM2LzMvaWNjLXdv/cmxkLXQyMC1hdXN0/cmFsaWEtMjAyMC1s/b2dvLXBuZ19zZWVr/bG9nby0zNjQxODEu/cG5n'/>
         <Card  user='Vandana kri' age ={24} img ='https://imgs.search.brave.com/FwuU2U0ljBoftFBqh5_LQTG0FxU91GkPGw315QjRIXc/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMuc2Vla2xvZ28u/Y29tL2xvZ28tcG5n/LzIzLzIvaWNjLWNy/aWNrZXQtd29ybGQt/Y3VwLTIwMTEtbG9n/by1wbmdfc2Vla2xv/Z28tMjM4NTkwLnBu/Zw'/>
 
         
--- 04-Arra-props/src/App.jsx
+++ 04-Arra-props/src/App.jsx
@@ -1,34 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react"
+import {Bookmark} from 'lucide-react'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className ='parent'>
+
+        <div className="card">
+            <div className="top">
+                <img src="https://imgs.search.brave.com/8KgsRJzLVNqFn5YGp7ZkB16L1Znegr72N45yDZpUzKg/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly90NC5m/dGNkbi5uZXQvanBn/LzAyLzk0LzU0LzA3/LzM2MF9GXzI5NDU0/MDcyMl9WSXdTV3ly/VTZxNzgxU2tIV1Ns/elVEWGpmb3VSbGo0/Ny5qcGc" alt="" />
+                <button> Save <Bookmark size={17 }/> </button>
+            </div>
+            <div className="center">
+                <h3>Amazone <span >5 Days Ago</span></h3>
+                <h2>Senior UI/UX Designer</h2>
+                <div className="tag">
+                <h4>Part-Time</h4>
+                <h4>Secinor Level</h4>
+                </div>
+            </div>
+            <div className="bottom">
+            <div className= 'btag'>
+                    <h3>$120/hr</h3>
+                    <p>Mumbi, Indians</p>
+            </div>
+                 <button>Apply Now </button>
+
+                </div>
+            </div>
+    </div>
+
+    
   )
 }
 
