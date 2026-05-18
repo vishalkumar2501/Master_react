fd8416f2628660a2365bc84b7e6b71551af0404e
Vishal Kumar <vishalkumar2501kvs@gmail.com>
2026-05-18 23:50:46 +0530

    add new fuctionslity

---
+++ project_based-L/src/components/Loader.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Loader = () => {
+
+  return (
+    <div className="loaderContainer">
+
+      <div className="loader"></div>
+
+      <h2>Loading...</h2>
+
+    </div>
+  );
+};
+
+export default Loader;
