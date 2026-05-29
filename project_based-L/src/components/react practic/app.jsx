a01d753619395ba3fbffd6d0e25363e38c47fb87
Vishal Kumar <vishalkumar2501kvs@gmail.com>
2026-05-29 23:17:58 +0530

    logic

--- project_based-L/src/components/react practic/app.jsx
+++ project_based-L/src/components/react practic/app.jsx
@@ -1,2 +1,4 @@
    import React, { useState } from "react"; function App() {
      // State for background color const [bgColor, setBgColor] = useState("white");   
+
+     return ( <div style={{ backgroundColor: bgColor, height: "100vh", textAlign: "center", paddingTop: "50px" }} ></div>
