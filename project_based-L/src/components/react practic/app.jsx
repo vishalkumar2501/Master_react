d952c8ba733ed19b4ae6da04eba4602d3a842ad1
Vishal Kumar <vishalkumar2501kvs@gmail.com>
2026-05-29 23:18:30 +0530

    logic

--- project_based-L/src/components/react practic/app.jsx
+++ project_based-L/src/components/react practic/app.jsx
@@ -2,3 +2,5 @@
      // State for background color const [bgColor, setBgColor] = useState("white");   
 
      return ( <div style={{ backgroundColor: bgColor, height: "100vh", textAlign: "center", paddingTop: "50px" }} ></div>
+
+        <h1>Background Color Changer</h1> <button onClick={() => setBgColor("red")}> Red </button> <button onClick={() => setBgColor("green")} style={{ marginLeft: "10px" }} > Green </button>
