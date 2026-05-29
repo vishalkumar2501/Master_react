b76497f1ff4e0876d068c70fe9d4a3f946868e5e
Vishal Kumar <vishalkumar2501kvs@gmail.com>
2026-05-29 23:19:36 +0530

    logic

--- project_based-L/src/components/react practic/app.jsx
+++ project_based-L/src/components/react practic/app.jsx
@@ -1,10 +1,52 @@
-   import React, { useState } from "react"; function App() {
-     // State for background color const [bgColor, setBgColor] = useState("white");   
 
-     return ( <div style={{ backgroundColor: bgColor, height: "100vh", textAlign: "center", paddingTop: "50px" }} ></div>
+import React, { useState } from "react";
 
-        <h1>Background Color Changer</h1> <button onClick={() => setBgColor("red")}> Red </button> <button onClick={() => setBgColor("green")} style={{ marginLeft: "10px" }} > Green </button>
+function App() {
 
+  // State for background color
+  const [bgColor, setBgColor] = useState("white");
 
+  return (
 
-        <button onClick={() => setBgColor("blue")} style={{ marginLeft: "10px" }} > Blue </button> <button onClick={() => setBgColor("white")} style={{ marginLeft: "10px" }} > Reset </button> </div> ); } export default App;
+    <div
+      style={{
+        backgroundColor: bgColor,
+        height: "100vh",
+        textAlign: "center",
+        paddingTop: "50px"
+      }}
+    >
+
+      <h1>Background Color Changer</h1>
+
+      <button onClick={() => setBgColor("red")}>
+        Red
+      </button>
+
+      <button
+        onClick={() => setBgColor("green")}
+        style={{ marginLeft: "10px" }}
+      >
+        Green
+      </button>
+
+      <button
+        onClick={() => setBgColor("blue")}
+        style={{ marginLeft: "10px" }}
+      >
+        Blue
+      </button>
+
+      <button
+        onClick={() => setBgColor("white")}
+        style={{ marginLeft: "10px" }}
+      >
+        Reset
+      </button>
+
+    </div>
+  );
+}
+
+export default App;
+
