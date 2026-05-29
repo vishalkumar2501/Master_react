577c3dff1193a260449e99e0fd641a36f022e60c
Vishal Kumar <vishalkumar2501kvs@gmail.com>
2026-05-29 23:19:03 +0530

    logic

--- project_based-L/src/components/react practic/app.jsx
+++ project_based-L/src/components/react practic/app.jsx
@@ -4,3 +4,7 @@
      return ( <div style={{ backgroundColor: bgColor, height: "100vh", textAlign: "center", paddingTop: "50px" }} ></div>
 
         <h1>Background Color Changer</h1> <button onClick={() => setBgColor("red")}> Red </button> <button onClick={() => setBgColor("green")} style={{ marginLeft: "10px" }} > Green </button>
+
+
+
+        <button onClick={() => setBgColor("blue")} style={{ marginLeft: "10px" }} > Blue </button> <button onClick={() => setBgColor("white")} style={{ marginLeft: "10px" }} > Reset </button> </div> ); } export default App;
