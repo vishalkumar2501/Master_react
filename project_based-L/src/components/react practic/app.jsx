cd6c6cde1ee2f4a440598161bd5c229baee009eb
Vishal Kumar <vishalkumar2501kvs@gmail.com>
2026-05-31 22:36:09 +0530

    logic

--- project_based-L/src/components/react practic/app.jsx
+++ project_based-L/src/components/react practic/app.jsx
@@ -1,1 +1,47 @@
-import React, { useState } from "react"; function App() { const [search, setSearch] = useState(""); const students = [ "Vishal", "Rahul", "Aman", "Priya", "Sneha", "Rohit", "Ankit" ];
+
+import React, { useState } from "react";
+
+function App() {
+
+  const [search, setSearch] = useState("");
+
+  const students = [
+    "Vishal",
+    "Rahul",
+    "Aman",
+    "Priya",
+    "Sneha",
+    "Rohit",
+    "Ankit"
+  ];
+
+  const filteredStudents = students.filter((student) =>
+    student.toLowerCase().includes(search.toLowerCase())
+  );
+
+  return (
+    <div style={{ padding: "30px" }}>
+
+      <h1>Search Filter App</h1>
+
+      <input
+        type="text"
+        placeholder="Search Student..."
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+
+      <hr />
+
+      <ul>
+        {filteredStudents.map((student, index) => (
+          <li key={index}>{student}</li>
+        ))}
+      </ul>
+
+    </div>
+  );
+}
+
+export default App;
+```
